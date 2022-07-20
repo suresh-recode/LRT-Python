15a93ab75f6dc55cfe7776fd516a00dd8a62916f
Suresh Arunachalam Thamilshelvan <Suresh Arunachalam@IRS-RPA-LW-0057.Recodesolutions.com>
2022-07-20 17:01:14 +0530

    Sync

---
+++ 01_comments.py
@@ -0,0 +1,9 @@
+# Single line comment
+
+"""
+Multi line comment 1
+Multi line comment 2
+"""
+
+# Multi line comment 1
+# Multi line comment 2
---
+++ 02_variables.py
@@ -0,0 +1,19 @@
+# int
+# float
+# # complex
+# list
+# tuple
+# set
+# dict
+# str
+# bool
+
+
+a_int = 10
+a_float = 20.1
+a_list = [1, True, 'Hai']
+a_tuple = (1, True, 'Hai')
+a_set = {1, True, 'Hai'}
+a_dict = {"Key": "Value"}
+a_bool = True
+a_str = 'Python'
