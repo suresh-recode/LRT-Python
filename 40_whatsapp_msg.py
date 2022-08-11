40e6e3263960e76d5f9944c331e0545c4fc1d43e
Suresh Arunachalam Thamilshelvan <Suresh Arunachalam@IRS-RPA-LW-0057.Recodesolutions.com>
2022-08-11 17:51:27 +0530

    Sync

--- 40_whatsapp_msg.py
+++ 40_whatsapp_msg.py
@@ -1,6 +1,23 @@
+# import pyautogui as pg
+# import time
+# time.sleep(5)
+# for i in range(10):
+#     pg.typewrite('Vijay Recode')
+#     pg.press('enter')
+
+
+import webbrowser
 import pyautogui as pg
 import time
-time.sleep(5)
-for i in range(10):
-    pg.typewrite('Vijay Recode')
-    pg.press('enter')
+whatsappphonenumber = '918778479731'
+data = 'Test'
+webbrowser.open('https://wa.me/{0}?text={1}'.format(whatsappphonenumber, data))
+time.sleep(2)
+pg.press(['tab'] * 8)
+time.sleep(2)
+pg.press('enter')
+time.sleep(2)
+pg.press(['tab'] * 2)
+pg.press('enter')
+time.sleep(20)
+pg.press('enter')
